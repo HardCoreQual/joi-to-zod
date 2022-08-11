f18764e728f1bedf03e287103e54d6e61d70f5b8
hardcorequal <hardcorequal@gmail.com>
2022-08-12 01:19:16 +0300

    feat: implement convertor for joi to zod

--- next.config.js
+++ next.config.js
@@ -1,7 +1,38 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    esmExternals: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    return {
+      ...config,
+      // webpack: {
+      //   configure: {
+      //     resolve: {
+      //       alias: {
+      //         // ignore the cut down browser distribution that
+      //         // joi's package.json steers webpack to
+      //         joi: path.resolve(__dirname, 'node_modules/joi/lib/index.js'),
+      //       },
+      //     },
+      //   },
+      // }
+
+    }
+  }
 }
 
-module.exports = nextConfig
+// next.config.js
+const removeImports = require("next-remove-imports")();
+module.exports = removeImports(nextConfig);
