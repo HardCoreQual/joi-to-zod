1f82bc99d832d576eda14744920ffd3b9ed6a556
hardcorequal <hardcorequal@gmail.com>
2022-08-13 20:57:34 +0300

    improve example

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,9 +18,20 @@ const CodeEditor = dynamic(
 
 function HomePage() {
   const [initial, setInitial] = React.useState(
-    `Joi.object().keys({ 
+    `enum Job {
+  Developer = "developer",
+  DevOps = "devops",
+  Designer = "designer",
+}
+
+Joi.object().keys({ 
   name: Joi.string().alphanum().min(3).max(30).required(),
   birthyear: Joi.number().integer().min(1970).max(2013), 
+  job: Joi.string().valid(Job.Developer, Job.DevOps, Job.Designer),
+
+  // NOTE: not all features are supported yet, for example: invert
+  nickname: joi.string().required().min(3).max(20).description('Nickname')
+    .regex(/^[a-z]+$/, { name: 'alpha', invert: true }),
 });`
   );
   const [final, setFinal] = React.useState(
